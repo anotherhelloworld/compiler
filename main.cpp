ec2db58128e72606f78d52350997d1d7dd4d7559
Махлярчук Андрей <helloworld106@gmail.com>
2017-03-09 22:36:28 -0800

    Fix command line arguments

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc == 1) {
-        std::cout << "error: no input file";
+    if (argc < 3) {
+        std::cout << "error: no input file" << std::endl;
     }
     if (argc == 3) {
         if (strcmp(argv[1], "-s") == 0) {
@@ -20,6 +20,8 @@ int main(int argc, char* argv[]) {
                 l = scanner.NextToken();
                 cout << left << setw(10) << l.pos.first << setw(10) <<l.pos.second << setw(25) << enumToStr[l.token] << l.val << setw(10) << endl;
             }
+        } else {
+            std::cout << "error: unknown argument" << std::endl;
         }
     }
     return 0;
