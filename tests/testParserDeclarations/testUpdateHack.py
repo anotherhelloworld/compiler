d61da565242c6a0afe82b308f3e2cd29d93ecdb8
Махлярчук Андрей <helloworld106@gmail.com>
2017-04-25 00:46:17 +1000

    Update tests

---
+++ tests/testParserDeclarations/testUpdateHack.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import pickle
+# subprocess.call(["cmake-build-debug/compiler", "tests/test001.in"])
+count = int(sys.argv[1])
+for i in range(0, count):
+    testnum = 'test' + '0' * (len(str(100)) - len(str(i))) + str(i)
+    fo = open(testnum + '.out', 'w')
+    ans = (subprocess.check_output(['/Users/a1/Documents/stuff/c++ stuff/compiler/bin/Debug/Compiler', '-d', '/Users/a1/Documents/stuff/c++ stuff/compiler/tests/testParserDeclarations/'
+     + testnum + '.in']))
+    fo.write(ans)
+    fo.close()
